e069ff59260984be5cfcd7c5c83f27a3c75db6fb
Sean Phelan <seanphelan97@gmail.com>
2018-09-25 12:35:55 -0600

    update seed util and node dependencies

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,22 +5,24 @@ const Note = require('../models/note');
 const Folder = require('../models/folder');
 const Tag = require('../models/tag');
 
-const seedNotes = require('../db/seed/notes');
-const seedFolders = require('../db/seed/folders');
-const seedTags = require('../db/seed/tags');
+const {notes, folders, tags} = require('../db/seed-data');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
     return Promise.all([
-      Note.insertMany(seedNotes),
-      Folder.insertMany(seedFolders),
-      Tag.insertMany(seedTags),
-      Folder.createIndexes(),
-      Tag.createIndexes()
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Tag.insertMany(tags)
     ]);
   })
-  .then(() => mongoose.disconnect())
+  .then(results => {
+    console.log('Inserted:')
+    console.log(results[0].length.toString(), 'notes');
+    console.log(results[1].length.toString(), 'folders');
+    console.log(results[2].length.toString(), 'tags');
+    mongoose.disconnect();
+  })
   .catch(err => {
     console.error(err);
   });
